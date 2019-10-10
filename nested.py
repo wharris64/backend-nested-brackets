2e55a4d7092c7e4056cdba6dba2784150df51972
WILL_HARRIS <wharris64@ivytech.edu>
2019-10-10 13:23:11 -0400

    fix flake 8

--- nested.py
+++ nested.py
@@ -6,10 +6,12 @@ Module docstring: One line description of what your program does.
 __author__ = "wharris64"
 
 import sys
-openers = [ "(", "[", "{", "<", "(*"]
+openers = ["(", "[", "{", "<", "(*"]
 closers = [")", "]", "}", ">", "*)"]
+
+
 def check_brackets(line):
-    currentbracket = []
+    # currentbracket = []
     temp_openers = []
     temp_closers = []
     counter = 0
@@ -23,71 +25,46 @@ def check_brackets(line):
             counter += 1
         elif line.startswith("<*"):
             token = "<*"
-            counter +=1
+            counter += 1
         else:
             counter += 1
             token = line[0]
         line = line[len(token):]
         if token in openers:
-            
             # currentbracket.append(token)
             temp_openers.append(openers.index(token))
         elif token in closers:
-            
             # currentbracket.append(token)
             temp_closers.append(closers.index(token))
     if temp_openers != temp_closers[::-1]:
-        no = "no"
+        # no = "no"
         counter -= 1
         counter = str(counter)
         return counter
     else:
         return('YES')
-        # start_line = line.startswith["(*"]
-        # end_line = line.startswith['*)']
-        # for char in line:
-        #     if char in openers:
-        #         currentbracket.append(char)
-        #         print(currentbracket)
-        #         temp_openers.append(openers.index(currentbracket[index])) 
-        #         index += 1
-        #     elif char in closers:
-        #         currentbracket.append(char)
-        #         temp_closers.append(closers.index(currentbracket[index]))
-        #         index += 1
-        
-        # if temp_openers == temp_closers[::-1]:
-        #     print("YES")
-        #     return "YES"
-        
-        # else:
-        #     # find index of error
-        #     print('No')
-        #     return "No" 
-        #         # and number
-        # # and number
 
 
 def read_file(filename):
     with open(filename) as reader, open("output.txt", "w")as writer:
-            content = reader.readlines()
-            # content = [x.strip( ) for x in content]
-            for i in content:
-                # check_brackets(i)
-                 writer.write(check_brackets(i) + "\n")
-    
+        content = reader.readlines()
+        # content = [x.strip( ) for x in content]
+        for i in content:
+            # check_brackets(i)
+            writer.write(check_brackets(i) + "\n")
+
 
 def main(args):
     """Add your code here"""
     read_file(args[1])
-    
+
 
 def reverse(s):
     if len(s) == 0:
         return s
     else:
         return reverse(s[1:]) + s[0]
-    
+
 
 if __name__ == '__main__':
     main(sys.argv)
